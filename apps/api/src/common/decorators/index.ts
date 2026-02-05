c9d3202ceea2f63aa1d2a40ef45bb38f485b4893
Adi <kresnadi@budisantoso.de>
2026-02-05 10:12:25 +0000

    chore(09-02): export permission and feature decorators
    - Add permissions.decorator.js to barrel export
    - Add feature.decorator.js to barrel export
    - All decorators available via ../decorators import

--- apps/api/src/common/decorators/index.ts
+++ apps/api/src/common/decorators/index.ts
@@ -1,3 +1,5 @@
 export * from './club-context.decorator.js';
 export * from './roles.decorator.js';
 export * from './super-admin.decorator.js';
+export * from './permissions.decorator.js';
+export * from './feature.decorator.js';
